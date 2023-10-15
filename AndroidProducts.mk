65f5b45534859b459e5f2855990dc0e2bae8d145
hbidcptrai <144416221+hbidcptrai@users.noreply.github.com>
2023-10-15 07:45:17 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosemary-user \
-    lineage_rosemary-userdebug \
-    lineage_rosemary-eng
+    elixir_rosemary-user \
+    elixir_rosemary-userdebug \
+    elixir_rosemary-eng
